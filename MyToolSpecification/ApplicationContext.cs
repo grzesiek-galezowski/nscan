3c4b8a9b43b5bd2e55d563fe1979bd27e623106f
Galezowski Grzegorz <grzesiek.galezowski@gmail.com>
2018-09-11 21:19:38 +0200

    Added new component test failing at NotImplementedException

---
+++ MyToolSpecification/ApplicationContext.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using MyTool.App;
+using MyTool.CompositionRoot;
+using MyTool.Xml;
+
+namespace MyTool
+{
+  public class ApplicationContext
+  {
+    private readonly ISupport _consoleSupport = new ConsoleSupport();
+    private readonly List<XmlProject> _xmlProjects = new List<XmlProject>();
+    private readonly List<(string, string)> _independentOfRules = new List<(string, string)>();
+    private Analysis _analysis;
+
+    public XmlProjectDsl HasProject(string assemblyName)
+    {
+      var xmlProject = new XmlProject()
+      {
+        AbsolutePath = AbsolutePathFor(assemblyName),
+        PropertyGroups = new List<XmlPropertyGroup>()
+        {
+          new XmlPropertyGroup()
+          {
+            AssemblyName = assemblyName
+          }
+        },
+        ItemGroups = new List<XmlItemGroup>()
+      };
+      _xmlProjects.Add(xmlProject);
+      return new XmlProjectDsl(xmlProject);
+    }
+
+    public static string AbsolutePathFor(string assemblyName)
+    {
+      return @"C:\" + assemblyName + ".cs";
+    }
+
+    public void AddIndependentOfRule(string dependingAssemblyName, string dependentAssemblyName)
+    {
+      _independentOfRules.Add((dependingAssemblyName, dependentAssemblyName));
+    }
+
+    public void StartAnalysis()
+    {
+      _analysis = Analysis.PrepareFor(_xmlProjects, _consoleSupport);
+
+      foreach (var (depending, dependent) in _independentOfRules)
+      {
+        _analysis.IndependentOfProject(depending, dependent);
+      }
+
+      _analysis.Run();
+    }
+
+    public void ReportShouldContainLine(string expected)
+    {
+      _analysis.Report.Should().Contain(expected + Environment.NewLine);
+      //todo what bout return code?
+    }
+  }
+
+  public class XmlProjectDsl
+  {
+    private readonly XmlProject _xmlProject;
+
+    public XmlProjectDsl(XmlProject xmlProject)
+    {
+      _xmlProject = xmlProject;
+    }
+
+    public void WithReferences(params string[] names)
+    {
+      _xmlProject.ItemGroups = new List<XmlItemGroup>
+      {
+        new XmlItemGroup() {ProjectReference = ProjectReferencesFrom(names)}
+      };
+    }
+
+    private static List<XmlProjectReference> ProjectReferencesFrom(string[] names)
+    {
+      return names.Select(n => new XmlProjectReference()
+      {
+        Include = ApplicationContext.AbsolutePathFor(n)
+      }).ToList();
+    }
+  }
+}
--- MyToolSpecification/ComponentSpecification.cs
+++ MyToolSpecification/ComponentSpecification.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using FluentAssertions;
-using MyTool.App;
-using MyTool.CompositionRoot;
-using MyTool.Xml;
-using TddXt.AnyRoot.Strings;
+﻿using TddXt.AnyRoot.Strings;
 using Xunit;
 using static TddXt.AnyRoot.Root;
 
@@ -29,7 +23,7 @@ namespace MyTool
       context.ReportShouldContainLine("[A] independentOf [B]: [OK]");
     }
 
-    /*[Fact]
+    [Fact]
     public void ShouldDetectDirectRuleBreak()
     {
       //GIVEN
@@ -39,64 +33,14 @@ namespace MyTool
       context.HasProject("C");
       context.HasProject("D");
 
-      context.AddDirectIndependentRule("A", "B");
-      
+      context.AddIndependentOfRule("A", "B");
+
       //WHEN
 
       context.StartAnalysis();
 
-
       //THEN
-      context.ReportShouldContain("Expected A to be independent of B, but found otherwise");
-
-    }*/
-  }
-
-  public class ApplicationContext
-  {
-    //bug remove private readonly Dictionary<ProjectId, IDotNetProject> _projectsById = new Dictionary<ProjectId, IDotNetProject>();
-    private readonly ISupport _consoleSupport = new ConsoleSupport();
-    private readonly List<XmlProject> _xmlProjects = new List<XmlProject>();
-    private readonly List<(string, string)> _independentOfRules = new List<(string, string)>();
-    private Analysis _analysis;
-
-    public void HasProject(string assemblyName)
-    {
-      _xmlProjects.Add(new XmlProject()
-      {
-        AbsolutePath = @"C:\" + assemblyName + ".cs",
-        PropertyGroups = new List<XmlPropertyGroup>()
-        {
-          new XmlPropertyGroup()
-          {
-            AssemblyName = assemblyName
-          }
-        },
-        ItemGroups = new List<XmlItemGroup>()
-      });
-    }
-
-    public void AddIndependentOfRule(string dependingAssemblyName, string dependentAssemblyName)
-    {
-      _independentOfRules.Add((dependingAssemblyName, dependentAssemblyName));
-    }
-
-    public void StartAnalysis()
-    {
-      _analysis = Analysis.PrepareFor(_xmlProjects, _consoleSupport);
-
-      foreach (var (depending, dependent) in _independentOfRules)
-      {
-        _analysis.IndependentOfProject(depending, dependent);
-      }
-
-      _analysis.Run();
-    }
-
-    public void ReportShouldContainLine(string expected)
-    {
-      _analysis.Report.Should().Contain(expected + Environment.NewLine);
-      //todo what bout return code?
+      context.ReportShouldContainLine("Expected A to be independent of B, but found otherwise");
     }
   }
 }
