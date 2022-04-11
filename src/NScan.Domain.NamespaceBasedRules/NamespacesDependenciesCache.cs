bef785d55f47b58fd7b27ab61bd687f414a4da99
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-04-11 23:15:25 +0200

    Update NamespacesDependenciesCache.cs

--- src/NScan.Domain.NamespaceBasedRules/NamespacesDependenciesCache.cs
+++ src/NScan.Domain.NamespaceBasedRules/NamespacesDependenciesCache.cs
@@ -145,12 +145,3 @@ public class NamespacesDependenciesCache : INamespacesDependenciesCache
     return !_dependenciesByNamespace.ContainsKey(namespaceName);
   }
 }
-
-public static class KeyValuePairDeconstruction
-{
-  public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
-  {
-    key = tuple.Key;
-    value = tuple.Value;
-  }
-}
