d8383f0dc0a263c8b14c345f737660c0bb05372d
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2023-12-30 19:49:37 +0100

    Update SourceCodeFileDto.cs

--- src/NScan.SharedKernel/ReadingSolution/Ports/SourceCodeFileDto.cs
+++ src/NScan.SharedKernel/ReadingSolution/Ports/SourceCodeFileDto.cs
@@ -4,18 +4,10 @@ using NScan.SharedKernel.ReadingCSharpSourceCode;
 
 namespace NScan.SharedKernel.ReadingSolution.Ports;
 
-public class SourceCodeFileDto(
-  RelativeFilePath filePathRelativeToProjectRoot,
-  Seq<string> declaredNamespaces,
-  string parentProjectRootNamespace,
-  string parentProjectAssemblyName,
-  Seq<string> usings,
-  Seq<ClassDeclarationInfo> classes)
-{
-  public Seq<ClassDeclarationInfo> Classes { get; } = classes;
-  public Seq<string> Usings { get; } = usings;
-  public string ParentProjectAssemblyName { get; } = parentProjectAssemblyName;
-  public string ParentProjectRootNamespace { get; } = parentProjectRootNamespace;
-  public RelativeFilePath PathRelativeToProjectRoot { get; } = filePathRelativeToProjectRoot;
-  public Seq<string> DeclaredNamespaces { get; } = declaredNamespaces;
-}
+public record SourceCodeFileDto(
+  RelativeFilePath PathRelativeToProjectRoot,
+  Seq<string> DeclaredNamespaces,
+  string ParentProjectRootNamespace,
+  string ParentProjectAssemblyName,
+  Seq<string> Usings,
+  Seq<ClassDeclarationInfo> Classes);
